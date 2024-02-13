c255fce356af55e1724032a29b1d082e5004a653
Shreyums <shreyasmakkikoppa@gmail.com>
2024-02-13 11:17:56 +0530

    second commit

---
+++ zegocloud-main/Exposys Lab Project/src/Room.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = 1294849945;
+    const serverSecret = "0b818367b11bce150cf6378702956e40";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "User1"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
